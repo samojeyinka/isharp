a4dc666a61ef2d40ad0c2f4dd0f8e83f426300e3
samojeyinka <samuelojeyinka@gmail.com>
2023-12-17 01:27:14 -1200

    Finish the CRUD Operations and the functionalities in the frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#index'
   get 'about', to: 'pages#about'
-  get 'help-support', to: 'pages#helpsupport'
-  get 'contact', to: 'pages#contact'
   resources :posts
 end
