bc219d795aa97db6af270b6ee00f7639e2a479d3
samojeyinka <samuelojeyinka@gmail.com>
2023-12-19 21:35:01 -1200

    Created user migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2023_12_18_104557) do
+ActiveRecord::Schema[7.2].define(version: 2023_12_20_085846) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -55,6 +55,13 @@ ActiveRecord::Schema[7.2].define(version: 2023_12_18_104557) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
