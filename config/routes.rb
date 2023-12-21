7fc05766ed2605cca9711d2cbefc9f603362d4b1
samojeyinka <samuelojeyinka@gmail.com>
2023-12-20 16:24:01 -1200

    The users functionalities continues

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   get 'help-support', to: 'pages#helpsupport'
   resources :posts
   resources :categories
+
+  get 'signup', to: 'users#new' 
+  resources :users, except: [:new]
+  
 end
